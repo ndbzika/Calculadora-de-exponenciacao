ef36c53f4f26b266be29800f123a6d40c6703802
Flávio Henrique <chakcpps@gmail.com>
2022-10-28 23:11:43 -0300

    :bug: fix: Lógica NaN e sem conteúdo

--- index.js
+++ index.js
@@ -8,32 +8,37 @@ const divResultado = document.querySelector("#divResultado");
 
 let totalDeVezes = 0;
 
-let calcularPotencia;
+let calcularPotencia = () => {
+  let valorBase = base.value;
+  let valorExpoente = expoente.value;
 
-botaoCalcular.addEventListener(
-  "click",
-  (calcularPotencia = () => {
-    let valorBase = base.value;
-    let valorExpoente = expoente.value;
+  let result = Math.pow(valorBase, valorExpoente);
 
-    let result = Math.pow(valorBase, valorExpoente);
+  let tituloRes = document.createElement("h3");
+  tituloRes.textContent = "Resultado:";
 
-    let conteudo = document.createElement("p");
-    conteudo.textContent = result;
-    let tituloRes = document.createElement("h3");
-    tituloRes.textContent = "Resultado:";
+  let conteudo = document.createElement("p");
+  conteudo.textContent = result;
+  if (base.value.length === 0 || expoente.value.length === 0) {
+    conteudo.textContent = "Digite algum valor!";
+  }
+  conteudo.classList.add("resultado");
 
-    botaoCalcular.addEventListener("click", () => {
-      if (totalDeVezes != 0) {
-        divResultado.removeChild(tituloRes);
-        divResultado.removeChild(conteudo);
-        totalDeVezes = 0;
-      }
-    });
+  if (isNaN(result)) {
+    conteudo.textContent = "Valor inválido!";
+  }
 
-    divResultado.appendChild(tituloRes);
-    divResultado.appendChild(conteudo);
-    totalDeVezes = totalDeVezes + 1;
-    console.log(totalDeVezes);
-  })
-);
+  divResultado.appendChild(tituloRes);
+  divResultado.appendChild(conteudo);
+  totalDeVezes = totalDeVezes + 1;
+
+  botaoCalcular.addEventListener("click", () => {
+    if (totalDeVezes !== 0) {
+      divResultado.removeChild(tituloRes);
+      divResultado.removeChild(conteudo);
+      totalDeVezes = 0;
+    }
+  });
+};
+
+botaoCalcular.addEventListener("click", calcularPotencia);
